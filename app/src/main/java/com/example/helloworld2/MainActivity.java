71a2afa90af6958b077679a824934b2bbfdeee32
Lee Scroggins <lee_scroggins@yahoo.com>
2021-09-21 15:58:17 -0600

    layout and buttons

--- app/src/main/java/com/example/helloworld2/MainActivity.java
+++ app/src/main/java/com/example/helloworld2/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.helloworld2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+  EditText name;
+  Button ok;
+  TextView result;
+  String userName;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    name = findViewById(R.id.editTextName);
+    ok = findViewById(R.id.buttonOk);
+    result = findViewById(R.id.textViewResult);
+
+    ok.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+
+        userName = name.getText().toString();
+        result.setText(userName);
+      }
+    });
   }
 }
